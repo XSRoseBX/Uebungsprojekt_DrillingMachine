46b7b7cc8da5e827ea75ee733ab90dc8d5d3da35
moushaya <hayamou2006@yahoo.com>
2022-11-09 15:01:57 +0100

    Initial commit - created in class on the 9th of November

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,6 +1,2 @@
 public class MainProgram {
-    // task 1b)
-    DrillingMachine dM1 = new DrillingMachine();
-    DrillingMachine dM2 = new DrillingMachine();
-    DrillingMachine dM3 = new DrillingMachine();
 }

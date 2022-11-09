fb3bc3f34e68fcb2863cf801be9332e6dad31836
XSRoseBX <moushaya@kkg-do.eu>
2022-11-09 20:33:09 +0100

    Homework to the 15th of November

---
+++ src/MainProgram.java
@@ -0,0 +1,6 @@
+public class MainProgram {
+    // task 1b)
+    DrillingMachine dM1 = new DrillingMachine();
+    DrillingMachine dM2 = new DrillingMachine();
+    DrillingMachine dM3 = new DrillingMachine();
+}
